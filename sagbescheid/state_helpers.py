0c188cd88aa2f67eee5782ccf0ad16861566fe69
Wieland Hoffmann <themineo@gmail.com>
2015-04-13 16:22:25 +0200

    reorder functions

--- sagbescheid/state_helpers.py
+++ sagbescheid/state_helpers.py
@@ -5,13 +5,13 @@
 from .state import State
 
 
-def is_recovery(old, new):
-    return old == State.failed and new == State.active
-
-
 def is_failure(state):
     return state == State.failed
 
 
 def is_ongoing_failure(old, new):
     return old == State.failed and new == State.failed
+
+
+def is_recovery(old, new):
+    return old == State.failed and new == State.active
